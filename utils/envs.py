5b65fd46c93df6afee4482a6ecca8e9574fa4c56
tongzj <1400721986@qq.com>
2023-06-23 15:34:29 +0800

    update

--- utils/envs.py
+++ utils/envs.py
@@ -6,24 +6,19 @@ from pathlib import Path
 
 def get_requirements(path=Path()):
     ''' 获取依赖包'''
-    import sys
     modules = {}
-    for m in list(sys.modules.values()):
+    for m in sys.modules.values():
         # 根据文件路径筛选
-        file = m.__dict__.get('__file__', None)
-        if file and 'site-packages' in file:
+        f = m.__dict__.get('__file__', None)
+        if f and 'site-packages' in f:
             # 根据导入方式筛选
             if 'SourceFileLoader' in str(m.__loader__):
                 # 根据隐藏模块前缀筛选
-                name = m.__name__.split('.', maxsplit=1)[0]
-                if name[0] != '_': modules[name] = m
-    # 筛除 site-packages 中的标准库
-    for name in ['pip', 'pkg_resources', 'setuptools', 'wheel']:
-        if name in modules: modules.pop(name)
-    modules = list(modules)
+                n = m.__name__.split('.', maxsplit=1)[0]
+                if n[0] != '_': modules[n] = m
     # 写入 txt 文本
     with open(path / 'requirements.txt', 'w') as f:
-        f.writelines(map(lambda s: s + '\n', modules))
+        f.writelines(map(lambda s: s + '\n', modules.keys()))
     return modules
 
 
@@ -33,8 +28,8 @@ class CondaEnv:
         self.env = os.popen('conda info --envs')
         self.version = sys.version_info[:3]
 
-    def create(self, name, version=None):
-        version = '.'.join(version if version else self.version)
+    def create(self, name, version=(3, 8, 0)):
+        version = '.'.join(map(str, version if version else self.version))
         os.system(f'conda create -n {name} python=={version}')
 
     def install(self, pack, pip=True, uninstall=False, upgrade=False):
@@ -54,15 +49,15 @@ class CondaEnv:
 
     def config(self):
         # 配置 pip
-        for key, value in {'timeout': 6000,
-                           'index-url': 'https://pypi.tuna.tsinghua.edu.cn/simple',
-                           'trusted-host': 'pypi.tuna.tsinghua.edu.cn'}.items():
-            os.system(f'pip config set global.{key} {value}')
+        for k, v in {'timeout': 6000,
+                     'index-url': 'https://pypi.tuna.tsinghua.edu.cn/simple',
+                     'trusted-host': 'pypi.tuna.tsinghua.edu.cn'}.items():
+            os.system(f'pip config set global.{k} {v}')
         # 配置 conda
-        for param in ('--add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/',
+        for p in ('--add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/',
                       '--add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/',
                       '--set show_channel_urls yes'):
-            os.system(f'conda config {param}')
+            os.system(f'conda config {p}')
 
     def __repr__(self):
         if not isinstance(self.env, str):
@@ -74,4 +69,4 @@ class CondaEnv:
 if __name__ == '__main__':
     env = CondaEnv()
     # env.jupyter(r'D:\Information\Python\Work_Space')
-    env.install('pot', pip=True)
+    # env.install('pot', pip=True)
