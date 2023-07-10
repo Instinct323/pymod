443f5794db83f26e16291cb594fda7cb86eb734f
tongzj <1400721986@qq.com>
2023-07-10 23:39:24 +0800

    update

--- utils/envs.py
+++ utils/envs.py
@@ -9,7 +9,7 @@ def git_push(repositories=(r'D:\Information\Python\mod',
                            r'D:\Information\Notes\info'),
              msg='update'):
     for repo in repositories:
-        os.chdir(repo), print('\n' * 2, repo)
+        os.chdir(repo), print('-' * 30 + '\n', repo)
         for cmd in ('git status', 'git add .',
                     f'git commit -m {msg}', 'git push main master'): os.system(cmd)
 
