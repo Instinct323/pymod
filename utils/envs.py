7698b3ee9b13f0f9e6624a9c0222b3916a15b2e0
TongZJ <1400721986@qq.com>
2023-12-28 23:27:10 +0800

    update on 2023-12-28 23:27:10.080906

--- utils/envs.py
+++ utils/envs.py
@@ -86,4 +86,4 @@ if __name__ == '__main__':
     os.chdir(os.getenv('dl'))
 
     # env.load_requirements(r'D:\Information\Python\mod\requirements.txt')
-    git_push(r'D:\Workbench\mod', r'D:\Information\Notes', r'D:\Information\Notes\info', r'D:\Workbench\Library')
+    git_push(r'D:\Workbench\mod', r'D:\Information\Notes', r'D:\Information\Data\Lib')
