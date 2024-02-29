6b811abba9192387ffcacbcad1de042f09798851
TongZJ <1400721986@qq.com>
2024-02-29 14:29:50 +0800

    update on 2024-02-29 14:29:43.683774

---
+++ ros2/node.py
@@ -0,0 +1,29 @@
+import sys
+
+# for ROS2
+if True:
+    import rclpy
+    from rclpy.node import Node
+# for CLion
+else:
+    from ros2 import rclpy
+    from ros2.rclpy.node import Node
+
+
+class NodeBase(Node):
+
+    @classmethod
+    def main(cls):
+        rclpy.init(args=sys.argv)
+        node = cls()
+        node.info(f'Successful initialization.')
+        rclpy.spin(node)
+        rclpy.shutdown()
+
+    def __init__(self, name='demo'):
+        super().__init__(name)
+        # log methods
+        self.info = self.get_logger().info
+        self.warn = self.get_logger().warn
+        self.error = self.get_logger().error
+        self.debug = self.get_logger().debug
