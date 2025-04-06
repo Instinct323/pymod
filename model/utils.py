1edea68e8b7d43cb52c6a9e54c2169210d1405c1
tongzj <tongzanjia@qq.com>
2025-04-07 01:10:53 +0800

    update on 2025-04-07 01:10:53.181599

---
+++ model/utils.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import PIL.Image
+
+
+def make_content(role, *contents) -> dict:
+    # text only
+    if isinstance(contents[0], str):
+        ret = contents[0]
+    # multiple contents
+    else:
+        ret = []
+        for t, v in contents:
+            if t == "image":
+                if isinstance(v, Path): v = f"file://{v}"
+                assert isinstance(v, PIL.Image.Image) or any(v.startswith(prefix) for prefix in ("http", "file://"))
+            elif t == "text":
+                assert isinstance(v, str)
+            else:
+                raise TypeError(f"Unsupported content type: {t}")
+            ret.append({"type": t, t: v})
+    return {"role": role, "content": ret}
